5762b96649ecd76d5d1b1f723c568757d320e075
Aymen Abdessalem <aymen.abdessalem.dev@gmail.com>
2019-08-03 20:45:58 -0400

    Generate controller and model Idea

--- src/idea/idea.entity.ts
+++ src/idea/idea.entity.ts
@@ -2,8 +2,12 @@ import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeor
 
 @Entity('idea')
 export class IdeaEntity {
-    @PrimaryGeneratedColumn('uuid') id: string;
-    @CreateDateColumn() created: Date;
-    @Column('text') idea: string;
-    @Column('text') description: string;
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+    @CreateDateColumn()
+    created: Date;
+    @Column('text')
+    idea: string;
+    @Column('text')
+    description: string;
 }
